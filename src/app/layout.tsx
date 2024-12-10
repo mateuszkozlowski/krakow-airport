3d8c3149e5a54293f96435a2c503780bf7523b51
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-10 12:18:22 +0100

    Revert "Update layout.tsx"
    This reverts commit 89e5e3a58c3ee48b126d0fb520d22d3fee1dd43b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
     if (TRACKING_ID) {
       ReactGA.initialize(TRACKING_ID);
       ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+    } else {
+      console.error('GA4 tracking ID is not defined');
     }
   }, []);
 
@@ -31,7 +33,10 @@ export default function RootLayout({
     };
 
     window.addEventListener('popstate', handleRouteChange);
-    return () => window.removeEventListener('popstate', handleRouteChange);
+    
+    return () => {
+      window.removeEventListener('popstate', handleRouteChange);
+    };
   }, []);
 
   return (
