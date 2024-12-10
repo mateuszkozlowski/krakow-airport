6ef6e9dd70dba6bbe9b353cf0aaac9ddb50c7b04
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-10 11:09:57 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  // Automatically set the basePath and assetPrefix for GitHub Pages deployment
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
-  assetPrefix = `/${repo}/`;  // for example, '/krakow-airport/'
-  basePath = `/${repo}`;       // for example, '/krakow-airport'
-}
-
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix,    // Ensures assets like images, CSS, and JS are correctly referenced
-  basePath,       // Ensures the app is served correctly from a subdirectory on GitHub Pages
-  output: 'export',  // Required to use static export
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
   images: {
-    unoptimized: true,  // If you're using images and not optimizing them
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
+
+export default nextConfig;
