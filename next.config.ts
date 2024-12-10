aff240c6766e672fd0f0287812b13175758ea258
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-10 09:42:48 +0100

    fixes (even more)

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix,
-  basePath,
-  output: 'export',
+  reactStrictMode: true, // Enable React strict mode
+  output: 'export', // Enable static export
+  trailingSlash: true, // Ensure trailing slash for all paths
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next.js image optimization
   },
 };
 
