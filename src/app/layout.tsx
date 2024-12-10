dd32699fd3d918bef68b9a3443e1da608ae70fa1
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-10 15:51:20 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// You can also move this before the component if you prefer
+const GA_ID = process.env.NEXT_PUBLIC_GA4_KEY || '';
+
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +16,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         {children}
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA4_KEY} />
+        {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
       </body>
     </html>
   )
