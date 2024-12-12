3b450b44ec2929aca5b4f0bbdfc3ee170a7c3ec2
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-12 13:58:48 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
 // src/app/layout.tsx
 import '@/app/globals.css';
-import { Inter } from 'next/font/google'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const gaId = process.env.NEXT_PUBLIC_GA4_KEY;
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2158235492134914"
-     crossorigin="anonymous"></script>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2158235492134914"
+        crossOrigin="anonymous"
+      ></script>
       <body className={inter.className}>
         {children}
       </body>
       {gaId && <GoogleAnalytics gaId={gaId} />}
     </html>
-  )
+  );
 }
