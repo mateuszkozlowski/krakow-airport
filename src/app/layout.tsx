4c5c453b24020206a89dc6a4faea282b4c8672af
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-16 20:49:21 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
         <meta name="google-adsense-account" content="ca-pub-2158235492134914" />
+        
+  <title>KRK.flights</title>
+  <meta name="description" content="Information on the status of flights from Krakow Airport, including delays and cancellations related to weather conditions." />
+  <meta name="keywords" content="Krakow, airport, app, flights, delays, cancellations, weather" />
+  <meta name="author" content="Mateusz Kozlowski" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       </head>
       <body className={inter.className}>
         {children}
