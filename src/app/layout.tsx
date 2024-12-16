49bd058418fa6aaa21c5e74be734f662e8700174
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-16 17:12:59 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,12 @@ export default function RootLayout({
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
+        <meta name="google-adsense-account" content="ca-pub-2158235492134914" />
       </head>
       <body className={inter.className}>
         {children}
         {gaId && <GoogleAnalytics gaId={gaId} />}
+        
       </body>
     </html>
   );
