674a0e56bc2e962074586db5970c72ca4176617d
Mateusz Kozłowski <mateusz.kozlowski@gmail.com>
2024-12-29 20:03:01 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,11 @@ import '@/app/globals.css';
 import { Inter } from 'next/font/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Clarity from '@microsoft/clarity';
-import Head from 'next/head';
+
+const projectId = "ploo7g9ey8"
 
 const inter = Inter({ subsets: ['latin'] });
-
-const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID;
-if (clarityProjectId) {
-  Clarity.init(clarityProjectId);
-}
+Clarity.init(projectId);
 
 export default function RootLayout({
   children,
@@ -21,19 +18,23 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <title>KRK.flights</title>
-        <meta name="description" content="Information on the status of flights from Krakow Airport, including delays and cancellations related to weather conditions." />
-        <meta name="keywords" content="Krakow, airport, app, flights, delays, cancellations, weather" />
-        <meta name="author" content="Mateusz Kozlowski" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <head>
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
-      </Head>
+        <meta name="google-adsense-account" content="ca-pub-2158235492134914" />
+        
+  <title>KRK.flights</title>
+  <meta name="description" content="Information on the status of flights from Krakow Airport, including delays and cancellations related to weather conditions." />
+  <meta name="keywords" content="Krakow, airport, app, flights, delays, cancellations, weather" />
+  <meta name="author" content="Mateusz Kozlowski" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      </head>
       <body className={inter.className}>
         {children}
-        {gaId && typeof window !== 'undefined' && <GoogleAnalytics gaId={gaId} />}
+        {gaId && <GoogleAnalytics gaId={gaId} />}
+        
       </body>
     </html>
   );
